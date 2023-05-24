17f3d2a7526e24029f60e211be199160e907ee1c
juliette <juliette2martinezc@gmail.com>
2023-05-24 10:42:21 -0500

    Login

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import React, { useState, useEffect } from 'react';
 import { getAllData } from './util/index';
+import { Login } from './components/Login';
+
 
 const URL = 'http://localhost:8000/api/v1/';
 
 function App() {
 
-const [message, setMessage] = useState(''); 
+  const [currentForm,setMessage]=useState('Login');
+  // const toggleForm=(forName) => {
+  //   setCurrentForm(forName);
+  // }
+console.log(currentForm);
 
   useEffect(() => {
 
@@ -13,18 +19,24 @@ const [message, setMessage] = useState('');
       const myData = await getAllData(URL)
       setMessage(myData.data);
     })();
-      
+  
     return () => {
+      
       console.log('unmounting');
-    }
 
+  
+    }
+ 
   }, []);
 
   return (
     <>
-      <h1>{message}</h1>
+    
+     <h1>{Login}</h1>
+      
     </>
   );
 }
 
 export default App;
+
