c38909c02a3c19d8c84f396e16a0431bd52ef1e2
juliette <juliette2martinezc@gmail.com>
2023-05-26 13:59:16 -0500

    new changes

--- src/index.js
+++ src/index.js
@@ -1,22 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
-import { BrowserRouter } from "react-router-dom";
+// import App from './App';
+import { BrowserRouter,Routes,Route } from "react-router-dom";
 import reportWebVitals from './reportWebVitals';
 import {Login} from './components/Login'
 import { Register } from './components/Register';
-import { Lost } from './components/Lost';
+import { ForgotPassword } from './components/ForgotPassword';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     // <BrowserRouter>
     //    <App />
-    // </BrowserRouter>,
     <BrowserRouter>
     <Routes>
-      <Route exact path='/'element={<Login/>}/> 
+      <Route exact path='/Login'element={<Login/>}/> 
       <Route exact path='/register'element={<Register/>}/> 
-      <Route exact path='/lost'element={<Lost/>}/> 
+      <Route exact path='/ForgotPassword'element={<ForgotPassword/>}/> 
     </Routes>
     </BrowserRouter>
 );
